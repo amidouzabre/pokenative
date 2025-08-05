767257542fd14f9aac42f9a5988911de86465ac2
amidou.zabre <amidou.zabre@gmail.com>
2025-08-05 14:47:44 +0000

    app: create index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+
+const styles = {
+  container: {
+    backgroundColor: 'red',
+    padding: 24,
+  }
+}
