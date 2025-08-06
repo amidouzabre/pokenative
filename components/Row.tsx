6a31ca13c2675a8f817f95949b3c40289479f9cb
amidou.zabre <amidou.zabre@gmail.com>
2025-08-06 15:28:25 +0000

    Components: create Row

---
+++ components/Row.tsx
@@ -0,0 +1,16 @@
+
+import { View, ViewStyle, type ViewProps } from "react-native";
+
+type Props = ViewProps & {
+    gap?: number;
+}
+
+export function Raw({style, gap, ...rest}:Props) {
+    return <View style={[rowStyle, style, gap ? {gap: gap} : undefined]} {...rest} />    
+}
+
+const rowStyle = {
+    flex: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+} satisfies ViewStyle;
