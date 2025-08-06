27688a99ad2d5e23770c2ec326873311da33a4a3
amidou.zabre <amidou.zabre@gmail.com>
2025-08-06 14:31:47 +0000

    hooks->useFetchQuery: add type

--- hooks/useFetchQuery.ts
+++ hooks/useFetchQuery.ts
@@ -2,17 +2,28 @@ import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 const endpoint = "https://pokeapi.co/api/v2/";
 
-export function useFetchQuery(path: string){
+type API = {
+    '/pokemon?limit=21': {
+        count: number,
+        next: string | null,
+        results: {
+            name: string,
+            url: string,
+        }[]
+    }
+}
+
+export function useFetchQuery<T extends keyof API>(path: T){
     return useQuery({
         queryKey: [path],
         queryFn: async () => {
             await wait(1)
-            return fetch(endpoint + path).then(res => res.json())
+            return fetch(endpoint + path).then(res => res.json() as Promise<API[T]>)
         }
     })
 }
 
-export function useInfinityFetchQuery(path: string){
+export function useInfinityFetchQuery<T extends keyof API>(path: T){
     return useInfiniteQuery({
         queryKey: [path],
         initialPageParam: endpoint + path,
@@ -22,7 +33,7 @@ export function useInfinityFetchQuery(path: string){
                 headers: {
                     Accept: "application/json",
                 }
-            }).then(res => res.json())
+            }).then(res => res.json() as Promise<API[T]>)
         },
         getNextPageParam: (lastPage) => {
             if("next" in lastPage){
