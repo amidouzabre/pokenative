2d9658fed7c76db42cf59f99fb0e3ff7905720c5
amidou.zabre <amidou.zabre@gmail.com>
2025-08-06 09:35:11 +0000

    app->index: import StyleSheet

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,49 @@
 import { Card } from "@/components/Card";
 import { ThemedText } from "@/components/ThemedText";
 import { useThemeColors } from "@/hooks/useThemeColors";
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const colors = useThemeColors();
+  const pokemons = Array.from({length:35}, (_, k) => ({
+    name: `Pokemon ${k+1}`,
+    id: k+1,
+  }))
   return (
     <SafeAreaView style={[styles.container, {backgroundColor: colors.tint}]}>
-      <Card>
-        <ThemedText variant="headline" color="grayDark">Pokédex</ThemedText>
+      <View style={styles.header}>
+        <Image source={require('@/assets/images/pokeball.png')} width={24} height={24} />
+        <ThemedText variant="headline" color="grayLight">Pokédex</ThemedText>
+      </View>
+      <Card style={styles.body}>
+        <FlatList
+          data={pokemons}
+          numColumns={3}
+          renderItem={({item}) => (
+            <Card>
+              <Text>{item.name}</Text>
+            </Card>
+          )} keyExtractor={item => item.id.toString()}
+        />
       </Card>
     </SafeAreaView>
   );
 }
 
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 4,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 16,
+    padding: 12
+  },
+  body: {
+    flex: 1,
   }
-};
+});
