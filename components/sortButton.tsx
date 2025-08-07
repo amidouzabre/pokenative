cd8d75b9fd1c556b652547a17c9c1130749f689f
amidou.zabre <amidou.zabre@gmail.com>
2025-08-07 21:14:42 +0000

    components->sortButton: create options

--- components/sortButton.tsx
+++ components/sortButton.tsx
@@ -1,24 +1,66 @@
 import { useThemeColors } from "@/hooks/useThemeColors";
-import { Image, Pressable, StyleSheet, View } from "react-native";
+import { useState } from "react";
+import { Image, Modal, Pressable, StyleSheet, View } from "react-native";
+import { Card } from "./Card";
+import { Row } from "./Row";
+import { ThemedText } from "./ThemedText";
 
 type Props = {
     value: "id" | "name";
     onChange: (v: "id" | "name") => void;
 }
 
+const options = [
+    {
+        label: 'Number',
+        value: 'id'
+    },
+    {
+        label: 'Name',
+        value: 'name'
+    }
+]
+
 export function SortButton({value, onChange}: Props) {
     const colors = useThemeColors();
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const onButtonPress = () => {
+       setIsModalVisible(true);
+    }
+    const onclose = () => {
+        setIsModalVisible(false);
+    }
     return (
-        <Pressable>
-            <View style={[styles.button, {backgroundColor: colors.grayWhite}]}>
-                <Image 
-                    source={
-                        value === 'id' ?
-                        require('@/assets/images/number.png') :
-                        require('@/assets/images/alpha.png')
-                    } width={16} height={16} />
-            </View>
-        </Pressable>
+        <>
+            <Pressable onPress={onButtonPress}>
+                <View style={[styles.button, {backgroundColor: colors.grayWhite}]}>
+                    <Image 
+                        source={
+                            value === 'id' ?
+                            require('@/assets/images/number.png') :
+                            require('@/assets/images/alpha.png')
+                        } width={16} height={16} />
+                </View>
+            </Pressable>
+            <Modal
+                transparent={true}
+                visible={isModalVisible}
+                onRequestClose={onclose}
+            >
+                <Pressable style={styles.backdrop} onPress={onclose}>
+                    <View style={[styles.popup, {backgroundColor: colors.tint}]}>
+                        <ThemedText style={styles.title} variant="subtitle2" color="grayLight">Sort by</ThemedText>
+                    </View>
+                    <Card style={styles.card}>
+                        {options.map(o => <Row key={o.value}>
+                            <View/>
+                            <ThemedText>{o.label}</ThemedText>
+                        </Row>)}
+                    </Card>
+                </Pressable>
+            </Modal>
+        </>
     )
 }
 
@@ -31,5 +73,23 @@ const styles = StyleSheet.create({
         flex: 0,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    backdrop: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)'
+    },
+    popup: {
+        padding: 4,
+        paddingTop: 16,
+        gap: 16,
+        borderRadius: 12,
+    },
+    title: {
+        paddingLeft: 20,
+    },
+    card: {
+        paddingVertical: 16,
+        paddingHorizontal: 20,
+        gap: 16,
     }
 });
