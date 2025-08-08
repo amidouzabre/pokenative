d511b65b544671c769ba1ae37bcefd0982e13db3
amidou.zabre <amidou.zabre@gmail.com>
2025-08-08 10:25:13 +0000

    app->pokemon->[id].tsx: create Header

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,13 +1,44 @@
+import { RootView } from "@/components/RootView";
+import { Row } from "@/components/Row";
+import { ThemedText } from "@/components/ThemedText";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, StyleSheet } from "react-native";
 
 export default function Pokemon() {
 
-    const params = useLocalSearchParams();
+    const params = useLocalSearchParams() as {id: string};
 
     return (
-        <View>
-        <Text>Pokémon {params.id}</Text>
-        </View>
+        <RootView>
+            <Row>
+                <Row>
+                    <Image 
+                        source={require('@/assets/images/back.png')} 
+                        width={32} 
+                        height={32} 
+                    />
+                    <ThemedText color="grayWhite" variant="headline">
+                        name
+                    </ThemedText>
+                </Row>
+                <Row>
+                    <ThemedText color="grayWhite" variant="subtitle2">
+                        #{params.id.padStart(3, '0')}
+                    </ThemedText>
+                </Row>
+            </Row>
+        </RootView>
     );
 }
+
+
+const styles = StyleSheet.create({
+  header: {
+    margin: 20,
+    justifyContent: 'space-between',
+  },
+  body: {
+    paddingHorizontal: 12,
+    paddingTop: 8,
+  },
+})
